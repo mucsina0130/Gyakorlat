768f92bb97a023073b5745f907f6cdf33f810493
mucsina0130 <mucsina0130@gmail.com>
2020-02-22 16:38:46 +0100

    Gyakorlati anyag:Junit

---
+++ src/main/java/BinrayCalculator.java
@@ -0,0 +1,14 @@
+public class BinrayCalculator {
+    String binaryAdd(String a, String b){
+        return null;
+    }
+    String binarySubtract(String a, String b){
+        return null;
+    }
+    String binaryMultiply(String a, String b){
+        return null;
+    }
+    String binaryDivide(String a, String b){
+        return null;
+    }
+}
---
+++ src/test/java/BinrayCalculatorTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinrayCalculatorTest {
+
+    BinrayCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new BinrayCalculator();
+    }
+
+    @AfterEach
+    void tearDown() {
+        calculator = null;
+    }
+
+    @Test
+    void binaryAdd() {
+        assertEquals(  "101", calculator.binaryAdd( "11", "10"));
+    }
+
+    @Test
+    void binarySubtract() {
+        assertEquals("11", calculator.binarySubtract("110", "11"));
+    }
+
+    @Test
+    void binaryMultiply() {
+        assertEquals("100", calculator.binaryMultiply("10","10"));
+    }
+
+    @Test
+    void binaryDivide() {
+        assertEquals("11", calculator.binaryDivide("1001","11"));
+    }
+}
